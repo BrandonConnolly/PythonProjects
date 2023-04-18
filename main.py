5693d89f4bd554e37c446b57be0f28e8e5f68fdb
BrandonConnolly <119525545+BrandonConnolly@users.noreply.github.com>
2023-04-18 18:49:27 -0230

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,32 @@
-#ABC Payroll
+#QAP 4 Graph
 #Author Brandon Connolly
-#Due Date Sept 23
 
-#Currant Rates & Taxes
-HrRate = 17.50
-ItemRate = 35
-Union = 15
-IncomeTax = .21
-CPP = .0495
-EI = .016
 
-#Input information
-EmployeeName = input("Enter Employee Name: ")
-HrsWorked = input("Enter Hours worked: ")
-HrsWorked = float(HrsWorked)
-ItemsProd = input("Enter Number of Products Produced: ")
-ItemsProd = float(ItemsProd)
-Bonus = input("Enter Total of Bonus Issued: ")
-Bonus = float(Bonus)
 
-#Calculations
+import matplotlib.pyplot as pit
+import matplotlib.pyplot as plt
 
-CommPay = ItemRate * ItemsProd
-HrPay = HrRate * HrsWorked
-Grosspay = HrPay + CommPay + Bonus
-IncomeDed = Grosspay * IncomeTax
-CPPDed = Grosspay * CPP
-EIDed = Grosspay * EI
-TotalDed = IncomeDed + CPPDed + EIDed + Union
-NetPay = Grosspay - TotalDed
+JanCl = input("Please Enter Claim Amount For Jan: ")
+FedCl = input("Please Enter Claim Amount For Feb: ")
+MarCl = input("Please Enter Claim Amount For Mar: ")
+AprCl = input("Please Enter Claim Amount For Apr: ")
+MayCl = input("Please Enter Claim Amount For May: ")
+JunCl = input("Please Enter Claim Amount For Jun: ")
+JulCl = input("Please Enter Claim Amount For Jul: ")
+AugCl = input("Please Enter Claim Amount For Aug: ")
+SepCl = input("Please Enter Claim Amount For Sep: ")
+OctCl = input("Please Enter Claim Amount For Oct: ")
+NovCl = input("Please Enter Claim Amount For Nov: ")
+DecCl = input("Please Enter Claim Amount For Dec: ")
 
-#Printed Information
-print(f"   Employee Name: {EmployeeName:<8s}")
-ItemsProd = "{:,.2f}".format(ItemsProd)
-print(f"   Total Products Produced: {ItemsProd:<4s}")
-HrPay = "${:,.2f}".format(HrPay)
-print(f"   Total Hourly Pay: {HrPay:<9s}")
-CommPay = "${:,.2f}".format(CommPay)
-print(f"   Total Commission: {CommPay:<9s}")
-Grosspay = "${:,.2f}".format(Grosspay)
-print(f"   Gross Pay: {Grosspay:<9s}")
-IncomeDed = "${:,.2f}".format(IncomeDed)
-print(f"   Total Income Tax: {IncomeDed:<9s}")
-CPPDed = "${:,.2f}".format(CPPDed)
-print(f"   Total CPP Contribution: {CPPDed:<9s}")
-EIDed = "${:,.2f}".format(EIDed)
-print(f"   Total EI Contribution: {EIDed:<9s}")
-Union = "${:,.2f}".format(Union)
-print(f"   Union Dues: {Union:<3s}")
-TotalDed = "${:,.2f}".format(TotalDed)
-print(f"   Total Deductions: {TotalDed:<9s}")
-NetPay = "${:,.2f}".format(NetPay)
-print(f"   Total Net Pay: {NetPay:<10s}")
+
+x_axis = ["Jan", "Fed", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+y_axis = [JanCl, FedCl, MarCl, AprCl, MayCl, JunCl, JulCl,  AugCl, SepCl, OctCl, NovCl, DecCl]
+
+plt.plot(x_axis , y_axis)
+plt.xlabel("Month of Year")
+plt.ylabel("Amount of Claims($)")
+plt.title("Yearly Claim Amounts($)")
+plt.grid(True)
+
+plt.show()
